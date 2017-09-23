3d39fde9e1b164ad1e562766a3328173418b6624
AON <18503158066@163.com>
2017-09-24 02:39:16 +0800

    Add addwin function.

--- doublewin.py
+++ doublewin.py
@@ -1,4 +1,24 @@
 class DoubleWin():
-
+	
+	TEAM_NAME = 0
+	TEAM_BITMAP = 1
+	
 	def _init_(self, list):
-		self.teamList = list
+		#get team list
+		self.__teamList = list
+		#get mask for caculate upper bracket
+		seed = list[-1]
+		self.__mask = seed[TEAM_BITMAP]
+	
+	def addWin(self, winner, loser):
+		#second member in every team will be set to be 2 after first initailization , 
+		#and the first member is the name of that team. 
+		for team in self.__teamList:
+			if team[TEAM_NAME] == winner:
+				#if a team wins a total bo, set a bit signal
+				temp = team[TEAM_BITMAP] * 2
+				team[TEAM_BITMAP] |= temp
+			else if team[TEAM_NAME] == loser:
+				#if it lose one game, set lower bracket bit on ---- last bit stand for lower bracket
+				temp = team[TEAM_BITMAP] | 1
+			
