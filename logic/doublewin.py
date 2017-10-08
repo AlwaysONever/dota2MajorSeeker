219d95e0f000bc930360528245b9a8e69f15fe3c
AON <18503158066@163.com>
2017-10-08 22:04:02 +0800

    recorrect some python problems

--- logic/doublewin.py
+++ logic/doublewin.py
@@ -1,104 +1,101 @@
 import json
 import csv
 
+
 class DoubleWin():
-	
-	TEAM_NAME = 0
-	TEAM_BITMAP = 1
-	TEAM_POINT = 2
-	UPPER_BRACKET = 1
-	POINT_MASK = 16
-	
-	def _init_(self, list):
-		#get team list
-		self.__teamList = list
-		#get mask for caculate upper bracket
-		seed = list[-1]
-		self.__mask = seed[DoubleWin.TEAM_BITMAP]
-		self.__resultList = None
-		self.__turnsDict = []
-		i = 2
-		j = 1
-		while i <= self.__mask:
-			self.__turnsDict[i] = j
-			j += 1
-			i = i << 1
-		
-	
-	def addWin(self, winner, winp, loser, losp):
-		#second member in every team will be set to be 2 after first initailization , 
-		#and the first member is the name of that team. 
-		for team in self.__teamList:
-			if team[DoubleWin.TEAM_NAME] == winner:
-				#if a team wins a total bo, set a bit signal
-				temp = team[DoubleWin.TEAM_BITMAP] * 2
-				team[DoubleWin.TEAM_BITMAP] |= temp
-				#point high 4 bits stand for lose games, and low 4 bits stand for win games
-				team[DoubleWin.TEAM_POINT].append(winp)
-			elif team[DoubleWin.TEAM_NAME] == loser:
-				#if it lose one game, set lower bracket bit on ---- last bit stand for lower bracket
-				temp = team[DoubleWin.TEAM_BITMAP] | DoubleWin.UPPER_BRACKET
-				team[DoubleWin.TEAM_POINT].append(losp)
-			
-	def end(self):
-		return self.__teamList
-		
-	def endAndGen(self):
-		#generate schedlue
-		resultList = []
-		for value in range(1,self.__mask):
-			bracket = []
-			for team in self.__teamList:
-				if team[DoubleWin.TEAM_BITMAP] == value:
-					bracket.append(team)
-			resultList.append(bracket)
-		self.__resultList = resultList
-		return resultList
-		
-	def storeInJSON(self, fileName):
-		if self.__resultList == None:
-			return False
-		resuilt = self.endAndGen()
-		with open(fileName, "w") as outStream:
-			json.dump(outStream, resuilt)
-		return True
-	def storeInCSV(self, fileName):
-		if self.__resultList == None:
-			return False
-		data = []
-		header = ["team", "turn", "win", "winrate"]
-		data.append(header)
-		dict = self.__turnsDict
-		for li in self.__resultList:
-			row = []
-			row.append(li[DoubleWin.TEAM_NAME])
-			bitmap = li[DoubleWin.TEAM_BITMAP]
-			mask = bitmap & self.__mask
-			bracket = bitmap & DoubleWin.UPPER_BRACKET
-			if bracket == DoubleWin.UPPER_BRACKET:
-				turn = "upper bracket" + str(self.__turnsDict[mask])
-			else:
-				turn = "lower bracket" + str(self.__turnsDict[mask])
-			row.append(turn)
-			
-			ponits = li[DoubleWin.TEAM_POINT]
-			point = 0 
-			loss = 0
-			for p in ponits:
-				lp = p >> 8
-				lp &= mask
-				p &= mask
-				point += p
-				loss += lp
-			row.append(point)
-			plus = point + loss
-			winrate = loss / plus
-			row.append(winrate)
-			data.append(row)
-		with open(fileName, "w") as outStream:
-			w = csv.reader(outStream)
-			w.writerows(data)
-			
-		
-		
-		
+    TEAM_NAME = 0
+    TEAM_BITMAP = 1
+    TEAM_POINT = 2
+    UPPER_BRACKET = 1
+    POINT_MASK = 16
+
+    def _init_(self, list):
+        # get team list
+        self.__teamList = list
+        # get mask for caculate upper bracket
+        seed = list[-1]
+        self.__mask = seed[DoubleWin.TEAM_BITMAP]
+        self.__resultList = None
+        self.__turnsDict = []
+        i = 2
+        j = 1
+        while i <= self.__mask:
+            self.__turnsDict[i] = j
+            j += 1
+            i = i << 1
+
+    def addWin(self, winner, winp, loser, losp):
+        # second member in every team will be set to be 2 after first initailization ,
+        # and the first member is the name of that team.
+        for team in self.__teamList:
+            if team[DoubleWin.TEAM_NAME] == winner:
+                # if a team wins a total bo, set a bit signal
+                temp = team[DoubleWin.TEAM_BITMAP] * 2
+                team[DoubleWin.TEAM_BITMAP] |= temp
+                # point high 4 bits stand for lose games, and low 4 bits stand for win games
+                team[DoubleWin.TEAM_POINT].append(winp)
+            elif team[DoubleWin.TEAM_NAME] == loser:
+                # if it lose one game, set lower bracket bit on ---- last bit stand for lower bracket
+                temp = team[DoubleWin.TEAM_BITMAP] | DoubleWin.UPPER_BRACKET
+                team[DoubleWin.TEAM_POINT].append(losp)
+
+    def end(self):
+        return self.__teamList
+
+    def endAndGen(self):
+        # generate schedlue
+        resultList = []
+        for value in range(1, self.__mask):
+            bracket = []
+            for team in self.__teamList:
+                if team[DoubleWin.TEAM_BITMAP] == value:
+                    bracket.append(team)
+            resultList.append(bracket)
+        self.__resultList = resultList
+        return resultList
+
+    def storeInJSON(self, fileName):
+        if self.__resultList == None:
+            return False
+        resuilt = self.endAndGen()
+        with open(fileName, "w") as outStream:
+            json.dump(outStream, resuilt)
+        return True
+
+    def storeInCSV(self, fileName):
+        if self.__resultList == None:
+            return False
+        data = []
+        header = ["team", "turn", "win", "winrate"]
+        data.append(header)
+        dict = self.__turnsDict
+        for li in self.__resultList:
+            row = []
+            row.append(li[DoubleWin.TEAM_NAME])
+            bitmap = li[DoubleWin.TEAM_BITMAP]
+            mask = bitmap & self.__mask
+            bracket = bitmap & DoubleWin.UPPER_BRACKET
+            if bracket == DoubleWin.UPPER_BRACKET:
+                turn = "upper bracket" + str(self.__turnsDict[mask])
+            else:
+                turn = "lower bracket" + str(self.__turnsDict[mask])
+            row.append(turn)
+
+            ponits = li[DoubleWin.TEAM_POINT]
+            point = 0
+            loss = 0
+            for p in ponits:
+                lp = p >> 8
+                lp &= mask
+                p &= mask
+                point += p
+                loss += lp
+            row.append(point)
+            plus = point + loss
+            winrate = loss / plus
+            row.append(winrate)
+            data.append(row)
+        with open(fileName, "w") as outStream:
+            w = csv.writer(outStream)
+            w.writerows(data)
+        return False
--- logic/team.py
+++ logic/team.py
@@ -1,67 +1,100 @@
 import json
 
+
 class team():
-	POS_CARRY = 1
-	POS_MID = 2
-	POS_OFFLINE = 4
-	POS_SUPPORT = 8
-	POS_HARDSUP = 16
-	POS_STANDIN = 0
-	OFF_CARRY = 0
-	OFF_MID = 1
-	OFF_OFFLINE = 2
-	OFF_SUPPORT = 3
-	OFF_HARDSUP = 4
-	OFF_STANDIN = 5
-	
-	def _init_(self, teamList):
-		if not isinstance(teamList, list):
-			return None
-		self.__players = {team.POS_CARRY: teamList[team.OFF_CARRY], team.POS_MID: teamList[team.OFF_MID],
-						  team.POS_OFFLINE: teamList[team.OFF_OFFLINE], team.POS_SUPPORT: teamList[team.OFF_SUPPORT],
-						  team.POS_HARDSUP: teamList[team.OFF_HARDSUP]}
-		stand = []
-		for li in teamList[team.OFF_STANDIN:]:
-			stand.append(li)
-		self.__players[team.POS_STANDIN] = stand
-	
-	def loadGameJSON(self, fileName):
-		with open(fileName) as outStream:
-			self.__gameDict = json.load(outStream)
-	
-	def addGame(self, gameDict):
-		#get win or lose status
-		winStatus = [gameDict["id"], gameDict["win"]]
-		self.__gameDict["winStatus"].append(winStatus)
-		if gameDict["win"] == "win":
-			self.__gameDict["won"] += 1
-		#get hero status
-		self.__gameDict["heros"].append(gameDict["heros"])
-		self.__gameDict["time"].append(gameDict["time"])
-		self.__gameDict["date"].append(gameDict["date"])
-		self.__gameDict["KDA"].append(gameDict["KDA"])
-		self.__gameDict["XPM"].append(gameDict["XPM"])
-		self.__gameDict["GPM"].append(gameDict["GPM"])
-			
-	def playerAdd(self, playerPos, playDict):
-		self.__players[playerPos].addGame(playDict)
-		
-	def getStyle(self):
-		return self.__gameDict["style"]
-		
-	def genStyle(self):
-		
-	def getWinRate(self):
-		
-	def getMajorPonit(self):
-	
-	def addMajorPonit(self, ponit):
-	
-	def storeInJSON(self, fileName):
-	
-	def storeInCSV(self, fileName):
-	
-	def getDict(self):
-	
-	
-		
+    POS_CARRY = 1
+    POS_MID = 2
+    POS_OFFLINE = 4
+    POS_SUPPORT = 8
+    POS_HARDSUP = 16
+    POS_STANDIN = 0
+    OFF_CARRY = 0
+    OFF_MID = 1
+    OFF_OFFLINE = 2
+    OFF_SUPPORT = 3
+    OFF_HARDSUP = 4
+    OFF_STANDIN = 5
+
+    def _init_(self, team_list, file_name):
+        if not isinstance(team_list, list):
+            return None
+        self.__players = {team.POS_CARRY: team_list[team.OFF_CARRY], team.POS_MID: team_list[team.OFF_MID],
+                          team.POS_OFFLINE: team_list[team.OFF_OFFLINE], team.POS_SUPPORT: team_list[team.OFF_SUPPORT],
+                          team.POS_HARDSUP: team_list[team.OFF_HARDSUP]}
+        stand = []
+        for li in team_list[team.OFF_STANDIN:]:
+            stand.append(li)
+        self.__players[team.POS_STANDIN] = stand
+
+        self.__gameDict = {"games": 0, "won": 0, "majorPoint": 0}
+        with open(file_name, "r") as out:
+            self.__gameDict = json.load(out)
+
+    def gen_game_dict(self, fileName):
+        game_dict = {"games":0, "won":0, "majorPoint":0}
+        with open(fileName, "w") as outStream:
+            json.dump()
+
+    def add_game(self, gameDict):
+        # get win or lose status
+        winStatus = [gameDict["id"], gameDict["win"]]
+        self.__gameDict["winStatus"].append(winStatus)
+        if gameDict["win"] == "win":
+            self.__gameDict["won"] += 1
+        # get hero status
+        self.__gameDict["heroes"].append(gameDict["heroes"])
+        self.__gameDict["time"].append(gameDict["time"])
+        self.__gameDict["date"].append(gameDict["date"])
+        self.__gameDict["KDA"].append(gameDict["KDA"])
+        self.__gameDict["XPM"].append(gameDict["XPM"])
+        self.__gameDict["GPM"].append(gameDict["GPM"])
+        self.__gameDict["games"] += 1
+
+    def player_add(self, player_pos, play_dict):
+        self.__players[player_pos].addGame(play_dict)
+
+    def get_style(self):
+        return self.__gameDict["style"]
+
+    def gen_style(self):
+        return None
+
+    def get_win_rate(self):
+        if self.__gameDict["games"] == 0:
+            return 0
+        return self.__gameDict["won"] / self.__gameDict["games"]
+
+    def get_major_point(self):
+        self.__gameDict["majorPoint"] = self.__players[team.POS_HARDSUP].getMP() \
+                                        + self.__players[team.POS_CARRY].getMP() + self.__players[
+                                            team.POS_OFFLINE].getMP() \
+                                        + self.__players[team.POS_MID].getMP() + self.__players[
+                                            team.POS_SUPPORT].getMP()
+        return self.__gameDict["majorPoint"]
+
+    def add_major_point(self, ponit):
+        for player in self.__gameDict["majorPoint"].keys():
+            self.__gameDict[player].addMP(ponit)
+
+    def store_in_json(self, fileName):
+        with open(fileName, "w") as out:
+            json.dump(self.__gameDict, out)
+
+    def store_in_csv(self, file_name):
+        data = []
+        header = ["id", "win", "heroes", "time", "date", "K", "D",
+                  "A", "GPM", "XPM"]
+        data.append(header)
+
+        for i in range(0, self.__gameDict["games"]):
+            row = [self.__gameDict["id"][i], self.__gameDict["win"][i], self.__gameDict["heroes"][i],
+                   self.__gameDict["time"][i], self.__gameDict["date"][i], self.__gameDict["K"][i][0],
+                   self.__gameDict["D"][i][1], self.__gameDict["A"][i][2], self.__gameDict["GPM"][i],
+                   self.__gameDict["XPM"][i]]
+            data.append(row)
+        with open(file_name, "w") as out:
+            w = csv.writer(out)
+            w.writerows(data)
+
+    def get_dict(self):
+        return self.__gameDicts
